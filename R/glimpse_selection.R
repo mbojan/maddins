f6991c6ed25d42ecc10f6efab69626b08486a7c4
Michal Bojanowski <michal2992@gmail.com>
2016-07-13 12:00:31 +0200

    Addins using glimpse and str

---
+++ R/glimpse_selection.R
@@ -0,0 +1,40 @@
+#' Glimpse at Object Selected in the Editor
+#'
+#'
+#'
+#' @name glimpse_selection
+NULL
+
+#' @rdname glimpse_selection
+#' @export
+glimpse_selection <- function() {
+  context <- rstudioapi::getActiveDocumentContext()
+  text <- context$selection[[1]]$text
+  dplyr::glimpse(get(text))
+}
+
+
+#' @rdname glimpse_selection
+#' @export
+str_selection <- function() {
+  context <- rstudioapi::getActiveDocumentContext()
+  text <- context$selection[[1]]$text
+  o <- get_or_not(text)
+  cat("Staring at", text, ":\n")
+  utils::str(o)
+}
+
+
+
+get_or_not <- function(objname) {
+  # Has white space?
+  has_space <- grepl("[[:space:]]", objname)
+  if(has_space) stop("selection contains a white space")
+  # Try to get()
+  tr <- try( get(objname) )
+  if( inherits(tr, "try-error") ) {
+    stop("cannot find object with name: ", objname)
+  } else {
+    return(tr)
+  }
+}
